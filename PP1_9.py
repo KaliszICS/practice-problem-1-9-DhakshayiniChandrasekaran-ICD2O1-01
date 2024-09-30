f672f9fcbf6b0b6cc41d7fb4eb03003a80a90fe1
DhakshayiniChandrasekaran-ICD2O1-01 <dhakshayini.chandrasekaran@student.tdsb.on.ca>
2024-09-30 16:41:06 +0000

    e

--- PP1_9.py
+++ PP1_9.py
@@ -1,20 +1,30 @@
 
+'''
+    Lesson: String Manipulation
+    Author: Dhakshayini
+    Date Created: Sept 30, 2024
+    Date Last Modified: Sept 30, 2024
+
+'''
 
 def q1(): 
   #Write Assignment code here
-
+  word = "hello world" 
 
 def q2(): 
   #Write Assignment code here
-
+  word = "Input a word"
+  print("Input a word")
+  print(word.lower())
+  print(word.upper())
 
 def q3(): 
   #Write Assignment code here
-
-
+  word = "Input a word that is at least 5 letters long"
+  print("Input a word that is at least 5 letters long")
 def q4(): 
   #Write Assignment code here
-
+  word = "Input a word"
 
 def q5(): 
   #Write Assignment code here
@@ -23,9 +33,10 @@ def q5():
 
 #Do not alter the following code
 #Comment out the following code when running your tests
-
+'''
 q1()
 q2()
 q3()
 q4()
 q5()
+'''
